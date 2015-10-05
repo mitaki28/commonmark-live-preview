f7ae94d7e4bb2fc93cc99b99b57441aced93bebb
mitaki28 <dr.ikros@gmail.com>
2015-10-05 20:07:34 +0900

    untabify and format

--- example/index.js
+++ example/index.js
@@ -8,9 +8,9 @@ var parser = new cmark.Parser();
 creator.onUpdate['CodeBlock'] = (node) => {
     var info_words = node.info ? node.info.split(/\s+/) : [];
     if (info_words.length > 0 && info_words[0].length > 0) {
-	node.dom.innerHTML = hljs.highlight(info_words[0], node.literal).value;
+        node.dom.innerHTML = hljs.highlight(info_words[0], node.literal).value;
     } else {
-	node.dom.innerHTML = hljs.highlightAuto(node.literal).value;
+        node.dom.innerHTML = hljs.highlightAuto(node.literal).value;
     }
 };
 
@@ -24,48 +24,48 @@ window.addEventListener('DOMContentLoaded', function() {
     var modeSelector = document.getElementById('mode');
     var mode = "none";
     var renderers = {
-	none: function(tree, preview) {
+        none: function(tree, preview) {
 
-	},
-	dom: function(tree, preview) {
-	    console.time('render(internal)');
-	    var dom = creator.create(tree);
-	    console.timeEnd('render(internal)');
-	    console.time('render(output)');
-	    removeChildren(preview);
-	    preview.appendChild(dom);
-	    console.timeEnd('render(output)');
-	},
-	html: function(tree, preview) {
-	    console.time('render(internal)');
-	    var html = htmlRenderer.render(tree);
-	    console.timeEnd('render(internal)');
-	    console.time('render(output)');
-	    preview.innerHTML = html;
-	    console.timeEnd('render(output)');
-	},
-	diff: function(tree, preview) {
-	    var dom = creator.update(tree);
-	    if (!preview.contains(dom)) {
-		removeChildren(preview);
-		preview.appendChild(dom);
-	    }
-	}
+        },
+        dom: function(tree, preview) {
+            console.time('render(internal)');
+            var dom = creator.create(tree);
+            console.timeEnd('render(internal)');
+            console.time('render(output)');
+            removeChildren(preview);
+            preview.appendChild(dom);
+            console.timeEnd('render(output)');
+        },
+        html: function(tree, preview) {
+            console.time('render(internal)');
+            var html = htmlRenderer.render(tree);
+            console.timeEnd('render(internal)');
+            console.time('render(output)');
+            preview.innerHTML = html;
+            console.timeEnd('render(output)');
+        },
+        diff: function(tree, preview) {
+            var dom = creator.update(tree);
+            if (!preview.contains(dom)) {
+                removeChildren(preview);
+                preview.appendChild(dom);
+            }
+        }
     };
     var render = function() {
-	console.log('rendering mode', mode);
-	console.time('parse');
-	var tree = parser.parse(text.value);
-	window.tree = tree;
-	console.timeEnd('parse');
+        console.log('rendering mode', mode);
+        console.time('parse');
+        var tree = parser.parse(text.value);
+        window.tree = tree;
+        console.timeEnd('parse');
 
-	console.time('render');
-	renderers[mode](tree, preview);
-	console.timeEnd('render');
+        console.time('render');
+        renderers[mode](tree, preview);
+        console.timeEnd('render');
     };
     modeSelector.addEventListener('change', function() {
-	mode = modeSelector.value;
-	render();
+        mode = modeSelector.value;
+        render();
     });
     text.addEventListener('keyup', render);
 });
